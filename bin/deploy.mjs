d4e77e165cb982098f2a1c0568d2bb5623dc66d4
Seth Battis <seth@battis.net>
2024-03-12 15:52:03 -0400

    update toolchain
    - use phive to manage tools
    - use partly-gcloudy for self-tidying app deploy
    - use twig for templating
    - improve ignores

---
+++ bin/deploy.mjs
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import gcloud from '@battis/partly-gcloudy';
+import cli from '@battis/qui-cli';
+
+await gcloud.init();
+await cli.init();
+try {
+  await gcloud.batch.appEngineDeployAndCleanup({ retainVersions: 2 });
+  cli.log.info('Deploy complete.');
+} catch (e) {
+  cli.log.error(e);
+}
