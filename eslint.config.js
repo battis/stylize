49a1421bb32c9bfadf0fd777005d5d8956defba0
Seth Battis <seth@battis.net>
2025-06-14 16:20:05 -0400

    chore: bump dependencies

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import config from '@battis/eslint-config';
+import globals from 'globals';
+
+export default {
+  ...config,
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      bootstrap: 'readonly'
+    }
+  }
+};
